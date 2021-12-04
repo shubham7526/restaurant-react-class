01c16883962ae10cdac78585178ebaf4a2f90c46
shubhamen1037 <shubhamen1037@gmail.com>
2021-12-04 14:29:16 +0530

    add all files

---
+++ src/Component/Button/Button.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react'
+
+
+class Button extends Component{
+
+    constructor(props){
+        super()
+    }
+
+    render(){
+
+        const { text, className, onClick, type, data }= this.props
+        return <div>
+            <button className={className} onClick={()=>onClick(type, data)}>{text}</button>
+        </div>
+    }
+}
+
+export default Button;
